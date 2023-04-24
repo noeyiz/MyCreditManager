c7556390ac01e43a5810dfca7e07fd70870575a5
noeyiz <lamoureclatant@gmail.com>
2023-04-24 21:59:34 +0900

    feat: add lower case

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -12,7 +12,6 @@ var loop = true
 
 while loop {
     print("원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
-    
     guard let input = readLine() else {
         print("입력이 잘못되었습니다.")
         break
